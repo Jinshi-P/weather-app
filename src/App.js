be5d39aeb26fd06474d64d45994a48a90cf8b1a5
Jinshi-P <134694718+Jinshi-P@users.noreply.github.com>
2026-02-03 09:56:02 +0300

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,128 +1,121 @@
-
-import { useState } from 'react';
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState(null);
   const [suggestions, setSuggestions] = useState([]);
 
   const apiKey = process.env.REACT_APP_WEATHER_API;
 
-  
+  const weatherData = async (city) => {
+    if (city) {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
+        );
+        const data = await response.json();
+        if (response.ok) {
+          setWeather(data);
+          setCity("");
+          setError("");
+        } else {
+          setError(data.message);
+        }
+      } catch (error) {
+        setError("Failed to fetch weather data");
+      }
+      setSuggestions([]);
+    } else {
+      setError("Please enter a city name");
+    }
+  };
 
   const fetchSuggestions = async (input) => {
     if (input.length > 2) {
       try {
-        const response = await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${input}`,
+        const response = await fetch(
+          `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${input}`,
           {
-            method: 'GET',
+            method: "GET",
             headers: {
-              'x-rapidapi-key': process.env.REACT_APP_RAPID_API,
-              'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
-            }
-          });
+              "x-rapidapi-key": process.env.REACT_APP_RAPID_API,
+              "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+            },
+          },
+        );
         const data = await response.json();
         if (response.ok) {
           setSuggestions(data.data);
         } else {
-          setWeather('')
+          setWeather("");
         }
-
-
       } catch (error) {
-        setError('Error fetching city suggestions')
+        setError("Error fetching city suggestions");
       }
-
     } else {
       setSuggestions([]);
-
     }
+  };
 
-
-  }
-
-  const handleSubmit = async (e) => {
-
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    setError('');
+    setError("");
     setWeather(null);
 
-    if (city) {
-      
-    try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-      );
-      const data = await response.json();
-      if (response.ok) {
-        setWeather(data);
-        setCity("");
-        setError('')
-        console.log(data)
-
-      } else {
-        setError(data.message)
-
-
-      }
-
-    } catch (error) {
-      setError('Failed to fetch weather data')
-
-
-    }
-      setSuggestions([])
-
-    } else {
-      setError('Please enter a city name')
-
-    }
-  }
+    weatherData(city);
+  };
 
   return (
-    <div className='app'>
-      <div className='container'>
+    <div className="app">
+      <div className="container">
         <form onSubmit={handleSubmit}>
-
-          <div className='search'>
-
-            <input type="text"
-              placeholder='Enter city...'
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Enter city..."
               value={city}
-              onChange={(e) => { setWeather(null); setCity(e.target.value); fetchSuggestions(e.target.value) }}
+              onChange={(e) => {
+                setError("");
+                setWeather(null);
+                setCity(e.target.value);
+                fetchSuggestions(e.target.value);
+              }}
             />
-            <button type='submit'> <FontAwesomeIcon icon={faSearch} size="lg" /></button>
+            <button type="submit">
+              {" "}
+              <FontAwesomeIcon icon={faSearch} size="lg" />
+            </button>
           </div>
 
           {/* to display suggestions */}
           <ul style={{ cursor: "pointer" }}>
             {suggestions.map((suggestion) => (
-              <li key={suggestion.id}
+              <li
+                key={suggestion.id}
                 onClick={() => {
-                  setCity(suggestion.city)
-                  setSuggestions([])
+                  setCity(suggestion.city);
+                  weatherData(suggestion.city);
+
+                  setSuggestions([]);
                 }}
-                style={{ listStyleType: "none" }}>
+                style={{ listStyleType: "none" }}
+              >
                 {suggestion.city}
               </li>
-            ))
-            }
+            ))}
           </ul>
         </form>
       </div>
-      <div className='displaybox'>
-        {
-          error && <p style={{ color: 'red', fontSize: '28px' }}>{error}</p>
-        }
-        {
-          weather &&
-          <div>
-            <img
+      <div className="displaybox">
+        {error && <p style={{ color: "red", fontSize: "28px" }}>{error}</p>}
+        {weather && (
+          <div style={{width:" 100%"}}>
+            <img 
               src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
               alt="Weather Icon"
             />
@@ -130,18 +123,18 @@ function App() {
 
             <h1>{weather.name}</h1>
 
-
             <h1>{weather.weather[0].description}</h1>
-            <div className='weather-details'>
-              <div className=''>
-                <p>Humidity: {weather.main.humidity}%</p></div>
-              <div className=''><p>Wind Speed: {weather.wind.speed} m/s</p></div>
+            <div className="weather-details" >
+              <div>
+                <p>{weather.main.humidity}%<br/>Humidity</p>
+              </div>
+              <div>
+                <p>{weather.wind.speed} m/s<br/>Wind Speed </p>
+              </div>
             </div>
-
           </div>
-        }
+        )}
       </div>
-
     </div>
   );
 }
